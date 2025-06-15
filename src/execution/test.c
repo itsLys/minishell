c204a6fecb7feef68429e4d46c4e51c3dc4a8594
ixchele <bengueddimez@gmail.com>
2025-06-15 21:54:39 +0100

    add the base of expand function

--- src/execution/test.c
+++ src/execution/test.c
@@ -1,15 +1,15 @@
 #include <minishell.h>
 
-// NOTE: extract_args(char **args);
+// NOTE: extract_args(char **args, t_env *env);
 
 void print_2d_array(char **array)
 {
-    int i = 0;
-    while (array[i])
-    {
-        printf("%s\n", array[i]);
-        i++;
-    }
+	int i = 0;
+	while (array[i])
+	{
+		printf("%s\n", array[i]);
+		i++;
+	}
 }
 
 void	erase(char *str, size_t index)
@@ -26,17 +26,18 @@ void	erase(char *str, size_t index)
 	}
 }
 
-bool	is_shell_var(char *str)
+bool	is_shell_var(const char *str)
 {
 	size_t i;
 
 	i = 0;
-	if (str[i] == '$' && str[i + 1] && ft_isalnum(str[i + 1]))
+	if (str[i] == '$' && str[i + 1]
+			&& (ft_isalnum(str[i + 1]) || str[i + 1] == '_'))
 		return (true);
 	return (false);
 }
 
-size_t	count_vars(char *str)
+size_t	count_vars(const char *str)
 {
 	size_t	i;
 	size_t	vars_counter;
@@ -52,34 +53,38 @@ size_t	count_vars(char *str)
 	return (vars_counter);
 }
 
-char	**find_vars(char *arg)
-{
-	size_t	i;
-	size_t	j;
-	size_t	nmb_of_vars;
-	char 	*var_start;
-	char	**vars;
-	
-	j = 0;
-	nmb_of_vars = count_vars(arg);
-	vars = ft_calloc(sizeof(char *), nmb_of_vars);
-	if (!vars)
-		return (NULL);
-	while (nmb_of_vars)
-	{
-		i = 0;
-		var_start = ft_strchr(arg, '$');
-		if (!var_start)
-			break ;
-		erase(var_start, 0);
-		while (var_start[i] && ft_isalnum(var_start[i]))
-			i++;
-		vars[j] = ft_strndup(var_start, i);
-		i++;
-		j++;
-	}
-	return (vars);
-}
+// char	**find_vars_in_str(char *arg)
+// {
+// 	size_t	i;
+// 	size_t	j;
+// 	size_t	nmb_of_vars;
+// 	char 	*var_start;
+// 	char	**vars;
+// 	char	*copy;
+//
+// 	j = 0;
+// 	nmb_of_vars = count_vars(arg);
+// 	vars = ft_calloc(sizeof(char *), nmb_of_vars + 1);
+// 	if (!vars)
+// 		return (NULL);
+// 	copy = ft_strdup(arg);
+// 	if (!copy)
+// 		return (NULL);
+// 	while (nmb_of_vars)
+// 	{
+// 		i = 0;
+// 		var_start = ft_strchr(copy, '$');
+// 		if (!var_start)
+// 			break ;
+// 		erase(var_start, 0);
+// 		while (var_start[i] && ft_isalnum(var_start[i]))
+// 			i++;
+// 		vars[j] = ft_strndup(var_start, i);
+// 		i++;
+// 		j++;
+// 	}
+// 	return (vars);
+// }
 
 void	append_env(t_env **head, t_env *new_node)
 {
@@ -96,7 +101,7 @@ void	append_env(t_env **head, t_env *new_node)
 	tmp->next = new_node;
 }
 
-t_env	*new_env_var(const char *name, const char *value)
+t_env	*new_env_var(const char *name, const char *value, bool exported)
 {
 	t_env *node;
 
@@ -105,6 +110,7 @@ t_env	*new_env_var(const char *name, const char *value)
 		return NULL;
 	node->name = ft_strdup(name);
 	node->value = ft_strdup(value);
+	node->is_exported = exported;
 	node->next = NULL;
 	return (node);
 }
@@ -133,72 +139,88 @@ void	free_env_list(t_env *env)
 	}
 }
 
-char	*search_env(char *var, t_env **env)
+char *get_env_value(t_env *env, const char *name)
 {
-	t_env	*head;
-
-	head = *env;
-	while (head)
+	while (env)
 	{
-		if (!ft_strcmp(head->name, var))
-			return (head->value);
-		head = head->next;
+		if (ft_strcmp(env->name, name) == 0)
+			return (env->value);
+		env = env->next;
 	}
-	return (NULL);
+	return (ft_strdup(""));
 }
 
-char	*env_var_name(char *str)
+size_t	sizeof_new_input(char *input, t_env *env)
 {
+	char	**vars;
+	size_t	new_size;
 	size_t	i;
 
 	i = 0;
-	while (str[i] && str[i] != '=')
-		i++;
-	return (ft_strndup(str, i));
-}
-
-char	*env_var_val(char *str)
-{
-	size_t	i;
-	char	*value;
-	
-	i = 1;
-	value = ft_strchr(str, '=');
-	while (value[i])
-
-
-}
-
-void	copy_env(t_env *env_list, char **env)
-{
-	size_t	i;
-	size_t	j;
-
-	while (env[i])
+	new_size = ft_strlen(input);
+	vars = find_vars_in_str(input);
+	while (vars[i])
 	{
-		while (env[i][j])
-		{
-				
-		}
+		new_size -= ft_strlen(vars[i]) + 1;
+		new_size += ft_strlen(get_env_value(env, vars[i++]));
 	}
+	return (new_size);
 }
 
-// TODO :
-// il faut que je crees une fonction qui recopie tout le env direct. 
-// de plus je dois diviser les VAR qui se composent de plusieurs mots tel que
-// VAR = mot1 mot2 mot3.
-// je dois creer une fonction qui verifie si le mask est double ou solitaire.
-// regrouper tout ce qui precede dans une seule fonction "extraxt_args"
-int main (int ac, char **av, char **env)
+char	*alloc_varname(char *input)
+{
+	size_t	i;
+	char	*varname;
+
+	i = 0;
+	while (ft_isalnum(input[i]) || input[i] == '_')
+		i++;
+	varname = ft_calloc(sizeof(char), i + 1);
+	return (varname);
+}
+
+char *expand_vars(char *input, t_env *env)
+{
+	char	*result;
+	char	*varname;
+	char	*value;
+	size_t	index[4];
+
+	ft_memset(index, 0, sizeof(index));
+	result = ft_calloc(sizeof(char) ,sizeof_new_input(input, env) + 1);
+	while (input[index[0]])
+	{
+		if (is_shell_var(&input[index[0]]))
+		{
+			index[0]++;
+			index[2] = 0;
+			index[3] = 0;
+			varname = alloc_varname(&input[index[0]]);
+			while (ft_isalnum(input[index[0]]) || input[index[0]] == '_')
+				varname[index[2]++] = input[index[0]++];
+			value = get_env_value(env, varname);
+			while (value[index[3]])
+				result[index[1]++] = value[index[3]++];
+			free(varname);
+			free(value);
+		}
+		else
+			result[index[1]++] = input[index[0]++];
+	}
+	return (result);
+}
+
+int main(void)
 {
 	t_env *env_list = NULL;
-
-	append_env(&env_list, new_env_var("VAR", "zakaria wa3r"));
-	append_env(&env_list, new_env_var("VAR1", "zakaria ma wa3rch"));
-	append_env(&env_list, new_env_var("VAR2", "zakaria "));
-	append_env(&env_list, new_env_var("VAR3", "hanane"));
-
-	print_env_list(env_list);
-	printf("val = %s\n", search_env("VAR1", &env_list));
+	append_env(&env_list, new_env_var("VAR", "zakaria", true));
+	append_env(&env_list, new_env_var("VAR1", "bonjour", true));
+	append_env(&env_list, new_env_var("VAR2", "		", true));
+	append_env(&env_list, new_env_var("VAR3", "monde", true));
+	append_env(&env_list, new_env_var("VAR4", "monde", true));
+	append_env(&env_list, new_env_var("VAR5", "monde", true));
+	char *s1 = expand_vars("$VAR$VAR2'est$VAR2'le$VAR2'roi'$VAR2'du$VAR2$VAR3", env_list);
+	printf("%s\n", s1);
+	free(s1);
 	free_env_list(env_list);
 }
