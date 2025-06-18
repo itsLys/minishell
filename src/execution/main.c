52fdb6a19112ccfb98b70b5ae55c87b496b15614
Lys <elyesseen@gmail.com>
2025-06-18 13:24:04 +0100

    x

--- src/execution/main.c
+++ src/execution/main.c
@@ -6,7 +6,7 @@
 /*   By: ihajji <ihajji@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 07:49:18 by ihajji            #+#    #+#             */
-/*   Updated: 2025/06/17 12:01:43 by ihajji           ###   ########.fr       */
+/*   Updated: 2025/06/18 13:20:42 by ihajji           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,8 +76,7 @@ int	execute_simple_command(t_ast_node *node, t_data *data, bool run_in_shell)
 		{
 			if (fork() == 0)
 				ft_execvpe(argv[0], argv, make_envp(data->env));
-			else
-				wait(NULL);
+			wait(NULL);
 		}
 	}
 	return (127);
@@ -100,48 +99,50 @@ void execute_first_pipeline(t_ast_node *pipeline, t_data *data, int pipefd[2])
 		dup2(pipefd[PIPE_WR], STDOUT_FILENO);
 		close(pipefd[PIPE_WR]);
 		close(pipefd[PIPE_RD]);
-		execute(pipeline->child, data, true);
+		exit(execute(pipeline->child, data, true));
 	}
-	// else
-	// 	perror("fork"); // maybe exit clean
+	else
+		perror("fork"); // maybe exit clean
 }
 
 void execute_middle_pipeline(t_ast_node *pipeline, t_data *data, int pipefd[2])
 {
-	// read from the read end of the pipe instead of stdout
-	// but only in child?
-	// so
-	// but also, write to the write end of a new pipe
+	pid_t pid;
+	int saved_stdin;
 
-	int saved = dup(STDIN_FILENO);
+	saved_stdin = dup(STDIN_FILENO);
+	dup2(pipefd[PIPE_RD], STDIN_FILENO);
 	close(pipefd[PIPE_WR]);
-	dup2(pipefd[PIPE_RD], 0);
 	close(pipefd[PIPE_RD]);
 	pipe(pipefd);
-	if (fork() == 0)
+	pid = fork();
+	if (pid == 0)
 	{
+		dup2(pipefd[PIPE_WR], STDOUT_FILENO);
 		close(pipefd[PIPE_RD]);
-		dup2(pipefd[PIPE_WR], 1);
 		close(pipefd[PIPE_WR]);
-		execute(pipeline->child, data, true);
+		exit(execute(pipeline->child, data, true));
 	}
-	dup2(saved, STDIN_FILENO);
-	close(saved);
+	else if (pid == ERROR)
+		perror("fork"); // maybe exit clean
+	dup2(saved_stdin, STDIN_FILENO);
+	close(saved_stdin);
 }
 
 pid_t execute_last_pipeline(t_ast_node *pipeline, t_data *data, int pipefd[2])
 {
 	pid_t pid;
+
 	pid = fork();
 	if (pid == 0)
 	{
 		dup2(pipefd[PIPE_RD], STDIN_FILENO);
 		close(pipefd[PIPE_WR]);
 		close(pipefd[PIPE_RD]);
-		execute(pipeline->child, data, true);
+		exit(execute(pipeline->child, data, true));
 	}
-	// else
-	// 	perror("fork"); // maybe exit clean
+	else if (pid == ERROR)
+		perror("fork"); // maybe exit clean
 	return pid;
 }
 
@@ -155,7 +156,6 @@ int	execute_pipeline(t_ast_node *node, t_data *data/* , bool run_in_shell */) //
 	pipeline = node;
 	if (is_single_pipeline(node))
 		return (execute(pipeline->child, data, false));
-	// printf("types\n");
 	execute_first_pipeline(pipeline, data, pipefd);
 	pipeline = pipeline->sibling;
 	while (pipeline && pipeline->type == G_PIPELINE
@@ -170,7 +170,6 @@ int	execute_pipeline(t_ast_node *node, t_data *data/* , bool run_in_shell */) //
 	waitpid(last, NULL, 0);
 	while (wait(NULL) != ERROR)
 		;
-	// printf("end types\n");
 	return 0;
 }
 
