2b68ac9e4f827502ec734716534485b23a239709
Lys <elyesseen@gmail.com>
2025-06-27 18:46:28 +0100

    set up redirection for subshell

--- src/execution/exec/subshell.c
+++ src/execution/exec/subshell.c
@@ -14,14 +14,23 @@
 
 int execute_subshell(t_ast_node *node, t_data *data)
 {
-	int pid;
-	int status;
+	int	pid;
+	int	std_io[2];
+	int	status;
 
+	std_io[STDOUT_FILENO] = dup(STDOUT_FILENO);
+	std_io[STDIN_FILENO] = dup(STDIN_FILENO);
+	if (node->child->sibling && setup_redir(node->child->sibling->child))
+		return FAILIURE;
 	pid = fork();
 	if (pid == 0)
 		exit(execute(node->child, data, true)); // clean exit
 	else if (pid == ERROR)
 		perror("fork"); // exit clean;
 	waitpid(pid, &status, 0);
+	dup2(std_io[STDOUT_FILENO], STDOUT_FILENO);
+	dup2(std_io[STDIN_FILENO], STDIN_FILENO);
+	close(std_io[STDOUT_FILENO]);
+	close(std_io[STDIN_FILENO]);
 	return WEXITSTATUS(status);
 }
